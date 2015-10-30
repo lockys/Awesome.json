1a1962c96cd129787f0911b212d9f740e4aa535c
lockys <locky4567@gmail.com>
2015-10-30 22:40:20 +0800

    add all json

--- parser/unify.js
+++ parser/unify.js
@@ -8,13 +8,6 @@ for (let i = 0, len = keyArr.length; i < len; ++i) {
   let listArr = awesome[keyArr[i]];
   for (let j = 0, lenList = listArr.length; j < lenList; ++j) {
     nammingMap[listArr[j].name] = listArr[j].name.replace(/\W/g, '').toLowerCase();
-    fs.writeFile('../output/' + listArr[j].name.replace(/\W/g, '').toLowerCase() + '.json', JSON.stringify(nammingMap, null, 2), (err) => {
-      if (!err) {
-        console.log('Success!');
-      } else {
-        console.error(err);
-      }
-    });
   }
 
 }
