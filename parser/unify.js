e4541a91692795173c2a876cb994a02b26f4287f
lockys <locky4567@gmail.com>
2015-12-01 12:27:11 +0800

    change name-map structure

--- parser/unify.js
+++ parser/unify.js
@@ -2,13 +2,13 @@
 var awesome = require('../output/awesome.json');
 var fs = require('fs');
 var keyArr = Object.keys(awesome);
-var nammingMap = {};
+var nammingMap = {forword: {}, backword: {}};
 
 for (let i = 0, len = keyArr.length; i < len; ++i) {
   let listArr = awesome[keyArr[i]];
   for (let j = 0, lenList = listArr.length; j < lenList; ++j) {
-    nammingMap[listArr[j].name] = listArr[j].name.replace(/\W/g, '').toLowerCase();
-    nammingMap[listArr[j].name.replace(/\W/g, '').toLowerCase()] = listArr[j].name;
+    nammingMap.forword[listArr[j].name] = listArr[j].name.replace(/\W/g, '').toLowerCase();
+    nammingMap.backword[listArr[j].name.replace(/\W/g, '').toLowerCase()] = listArr[j].name;
   }
 
 }
