003503d80deaa4939dbe59d88a1e9a090ffcc236
EC2 Default User <ec2-user@ip-172-31-29-126.us-west-2.compute.internal>
2015-12-11 17:48:27 +0000

    crob job

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var schedule = require('node-schedule');
 var exec = require('child_process').exec;
 var buildCMD = 'npm run build && npm run push';
 
-exec(buildCMD, function(error, stdout, stderr) {
-  if (!error) {
-    console.log(stdout);
-  }
+var j = schedule.scheduleJob('* * * * *', function(){
+  exec(buildCMD, function(error, stdout, stderr) {
+    if (!error) {
+      console.log(stdout);
+    }
+  });
 });
+
