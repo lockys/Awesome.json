260be826debfee321bd0d7885900ca288e1ff084
lockys <locky4567@gmail.com>
2015-12-11 18:06:38 +0000

    updated json

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var exec = require('child_process').exec;
 var buildCMD = 'npm run build && npm run push';
 console.log('start');
 //var j = schedule.scheduleJob('*/20 * * * *', function(){
+
   console.log('schedule!');
   exec(buildCMD, function(error, stdout, stderr) {
     if (!error) {
