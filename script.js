b7d3fcaf51b1f3e2a7e8d0d5e92cf1632958c429
lockys <locky4567@gmail.com>
2015-12-19 13:55:07 -0500

    timer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var schedule = require('node-schedule');
 var exec = require('child_process').exec;
 var buildCMD = 'npm run awesome && npm run build && npm run push';
 
-var j = schedule.scheduleJob('*/10 * * * * *', function(){
+var j = schedule.scheduleJob('* 56 2 * * *', function(){
   console.log('Go!');
   try {
    exec(buildCMD, function(error, stdout, stderr) {
