c67897bc63d11017b994fe71f362587ebb8b7363
lockys <locky4567@gmail.com>
2015-12-19 23:59:00 -0500

    updated json

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ var CronJob = require('cron').CronJob;
 var exec = require('child_process').exec;
 var buildCMD = 'npm run awesome && npm run build && npm run push';
 
-exec(buildCMD, function(error, stdout, stderr) {
-  if (!error) {
-    console.log(stdout);
-  }
-});
+try {
+  exec(buildCMD, function(error, stdout, stderr) {
+    if (!error) {
+      console.log(stdout);
+      console.log(stderr);
+    }
+  });
+} catch (e) {
+  console.error(e);
+}
 
 //new CronJob('00 42 12 * * 1-7', function() {
 //  console.log('You will see this message every second');
