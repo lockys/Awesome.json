599b1278e6700a4f41898ebfc1882d74a4919732
Calvin Jeng <locky4567@gmail.com>
2016-03-26 09:57:21 +0000

    update deps.

---
+++ app.js
@@ -0,0 +1,17 @@
+var CronJob = require('cron').CronJob;
+var exec = require('child_process').exec;
+var buildCMD = 'npm run awesome && npm run build && npm run push';
+
+console.log('Do The Crob Job! Awesome :)');
+new CronJob('00 00 * * */3 *', function() {
+  try {
+    exec(buildCMD, function(error, stdout, stderr) {
+      if (!error) {
+        console.log(stdout);
+        console.log(stderr);
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
