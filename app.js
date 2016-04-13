c78a76b0ced398f205f5845499df7b12136405ee
lockys <locky4567@gmail.com>
2016-04-13 00:45:57 -0400

    updated json

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var exec = require('child_process').exec;
 var buildCMD = 'npm run awesome && npm run build && npm run push';
 
 console.log('Do The Crob Job! Awesome :)');
-new CronJob('* * * * * *', function() {
+new CronJob('* */1 * * * *', function() {
   try {
     exec(buildCMD, function(error, stdout, stderr) {
+      console.log('executing ...');
       if (!error) {
         console.log(stdout);
         console.log(stderr);
