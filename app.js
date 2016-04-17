4d03ac82b830cbcf8ac72ccfaddeabfaf131e77a
lockys <locky4567@gmail.com>
2016-04-17 12:28:29 -0400

    updated json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var buildAllRepo = 'npm run build';
 var pushCmd = 'npm run push';
 
 console.log('Do The Crob Job! Awesome :)');
-new CronJob('* * */12 * * *', function() {
+new CronJob('* * */6 * * *', function() {
   try {
     exec(buildAwesome, finishAwesome);
   } catch (e) {
